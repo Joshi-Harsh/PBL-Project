2642ad12350aa64939e6642bb324e45c1078303a
khan mohammed <khanmohammed0028@gmail.com>
2024-04-17 17:15:44 +0530

    database

--- header.php
+++ header.php
@@ -1,3 +1,8 @@
+    <?php 
+    include("connection2.php");
+    include("signup.php")
+    
+    ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -76,112 +81,103 @@
       </div>
     </nav>
     <section class="home">
-        
-        <div class="form_container">
-            <div class="form login_form">
-              <section class="user">
+    <div class="form_container">
+        <div class="form login_form">
+            <section class="user">
                 <div class="user_options-container">
-                  <div class="user_options-text">
-                    <i class="material-symbols-outlined form_close"> close </i>
-                    <div class="user_options-unregistered">
-                      <h2 class="user_unregistered-title text-white">
-                        Don't have an account?
-                      </h2>
-                      <!-- <p class="user_unregistered-text">
-                        Banjo tote bag bicycle rights, High Life sartorial cray
-                        craft beer whatever street art fap.
-                      </p> -->
-                      <button class="user_unregistered-signup" id="signup-button">
-                        Sign up
-                      </button>
-                    </div>
-    
-                    <div class="user_options-registered">
-                      <h2 class="user_registered-title text-white">
-                        Have an account?
-                      </h2>
-                      <!-- <p class="user_registered-text">
-                        Banjo tote bag bicycle rights, High Life sartorial cray
-                        craft beer whatever street art fap.
-                      </p> -->
-                      <button class="user_registered-login" id="login-button">
-                        Login
-                      </button>
-                    </div>
-                  </div>
-    
-                  <div class="user_options-forms" id="user_options-forms">
-                    <div class="user_forms-login">
-                      <h2 class="forms_title">Login</h2>
-                      <form class="forms_form">
-                        <fieldset class="forms_fieldset">
-                          <div class="forms_field">
-                            <input
-                              type="email"
-                              placeholder="Email"
-                              class="forms_field-input"
-                              required
-                              autofocus />
-                          </div>
-                          <div class="forms_field">
-                            <input
-                              type="password"
-                              placeholder="Password"
-                              class="forms_field-input"
-                              required />
-                          </div>
-                        </fieldset>
-                        <div class="forms_buttons">
-                          <button type="button" class="forms_buttons-forgot">
-                            Forgot password?
-                          </button>
-                          <input
-                            type="submit"
-                            value="Log In"
-                            class="forms_buttons-action" />
+                    <div class="user_options-text">
+                        <i class="material-symbols-outlined form_close"> close </i>
+                        <div class="user_options-unregistered">
+                            <h2 class="user_unregistered-title text-white">
+                                Don't have an account?
+                            </h2>
+                            <button class="user_unregistered-signup" id="signup-button">
+                                Sign up
+                            </button>
                         </div>
-                      </form>
-                    </div>
-                    <div class="user_forms-signup">
-                      <h2 class="forms_title">Sign Up</h2>
-                      <form class="forms_form">
-                        <fieldset class="forms_fieldset">
-                          <div class="forms_field">
-                            <input
-                              type="text"
-                              placeholder="Full Name"
-                              class="forms_field-input"
-                              required />
-                          </div>
-                          <div class="forms_field">
-                            <input
-                              type="email"
-                              placeholder="Email"
-                              class="forms_field-input"
-                              required />
-                          </div>
-                          <div class="forms_field">
-                            <input
-                              type="password"
-                              placeholder="Password"
-                              class="forms_field-input"
-                              required />
-                          </div>
-                        </fieldset>
-                        <div class="forms_buttons">
-                          <input
-                            type="submit"
-                            value="Sign up"
-                            class="forms_buttons-action" />
+                        <div class="user_options-registered">
+                            <h2 class="user_registered-title text-white">
+                                Have an account?
+                            </h2>
+                            <button class="user_registered-login" id="login-button">
+                                Login
+                            </button>
                         </div>
-                      </form>
                     </div>
-                  </div>
+                    <div class="user_options-forms" id="user_options-forms">
+                        <div class="user_forms-login">
+                            <h2 class="forms_title">Login</h2>
+                            <form class="forms_form" action="index.php" onsubmit="return isvalid()" method="POST">
+                                <fieldset class="forms_fieldset">
+                                    <div class="forms_field">
+                                        <input
+                                            type="text"
+                                            name="email"
+                                            placeholder="Email"
+                                            class="forms_field-input"
+                                            required
+                                            autofocus />
+                                    </div>
+                                    <div class="forms_field">
+                                        <input
+                                            type="password"
+                                            name="password"
+                                            placeholder="Password"
+                                            class="forms_field-input"
+                                            required />
+                                    </div>
+                                </fieldset>
+                                <div class="forms_buttons">
+                                    <button type="button" class="forms_buttons-forgot">
+                                        Forgot password?
+                                    </button>
+                                    <input
+                                        type="submit"
+                                        value="Login"
+                                        class="forms_buttons-action"
+                                        name="submit" />
+                                </div>
+                            </form>
+                        </div>
+                        <div class="user_forms-signup" id="form">
+                            <h2 class="forms_title">Sign Up</h2>
+                            <form class="forms_form" action="signup.php" method="POST">
+                                <fieldset class="forms_fieldset">
+                                    <div class="forms_field">
+                                    <input type="text" id="user" name="user" placeholder="Enter Username" class="forms_field-input" required>
+                                        <!-- <input
+                                            type="text"
+                                            name="fullname"
+                                            placeholder="Full Name"
+                                            class=""
+                                            required /> -->
+                                    </div>
+                                    <div class="forms_field">
+                                    <input type="email" id="email" name="email" class="forms_field-input" placeholder="Enter Email" required>
+                                        
+                                    </div>
+                                    <div class="forms_field">
+                                    <input type="password" class="forms_field-input" id="pass" name="pass" placeholder="Create Password" required>
+                                        
+                                    </div>
+                                    <div class="forms_field">
+                                    <input type="password" id="cpass" name="cpass" placeholder="Retype Password" required>
+  </div>
+                                </fieldset>
+                                <div class="forms_buttons">
+                                <input type="submit" class="forms_buttons-action" id="btn" value="SignUp" name = "submit"/>
+                                    
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                   
                 </div>
-              </section>
-            </div>
-          </div>
-    </section>
+            </section>
+        </div>
+    </div>
+</section>
+
     <script
       src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
@@ -220,6 +216,24 @@ loginButton.addEventListener(
   },
   false
 );
+
+function isvalid(){
+                var user = document.form.user.value;
+                var pass = document.form.pass.value;
+                if(user.length=="" && pass.length==""){
+                    alert(" Username and password field is empty!!!");
+                    return false;
+                }
+                else if(user.length==""){
+                    alert(" Username field is empty!!!");
+                    return false;
+                }
+                else if(pass.length==""){
+                    alert(" Password field is empty!!!");
+                    return false;
+                }
+                
+            }
 
     </script>
 </body>
